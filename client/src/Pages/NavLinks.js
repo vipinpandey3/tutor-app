140089ae7a9f40718cc78752312f24d09e0806e1
Vipin Pandey <dev.virpandey@gmail.com>
2021-11-08 09:24:12 +0530

    sidenav and navbar changes

--- client/src/Pages/NavLinks.js
+++ client/src/Pages/NavLinks.js
@@ -1,28 +1,44 @@
+
+// import {GrHomeRounded} from 'react-icons/gr'
+import {GiTeacher, GiPapers, GiReceiveMoney} from 'react-icons/gi';
+import {HiHome, HiOutlineUsers} from 'react-icons/hi'
+
 const NavLinks = [
     {
         id: 1,
         name: "Dashboard",
-        path: "/dashboard"
+        path: "/dashboard",
+        icons: <HiHome />
     },
     {
         id: 2,
         name: "Tutors",
-        path: "/tutors"
+        path: "/tutors",
+        icons: <GiTeacher />
     },
     {
         id: 3,
         name: "Students",
-        path: "/students"
+        path: "/students",
+        icons: <HiOutlineUsers />
     },
     {
         id: 4,
         name: "Exams",
-        path: "/exams"
+        path: "/exams",
+        icons: <GiPapers />
     },
     {
         id: 5,
         name: 'Fees',
-        path: '/fees'
+        path: '/fees',
+        icons: <GiReceiveMoney />
+    },
+    {
+        id: 6,
+        name: 'Users',
+        path: '/users',
+        icons: <HiOutlineUsers />
     }
 ];
 

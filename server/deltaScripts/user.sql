36b816d445339ca7cbcf229980f9764be665a63f
vipin pandey <vipinpandey@vipins-Air.lan>
2023-12-17 20:40:01 +0530

    adding the user sql

---
+++ server/deltaScripts/user.sql
@@ -0,0 +1,2 @@
+INSERT INTO YourTableName (id, firstName, lastName, emailId, password, status, role, createdAt, updatedAt)
+VALUES (1, 'Vipin', 'Pandey', 'vipinpandey530@gmail.com', '$2a$08$BOW/Uco9szuQN3E6EkhMnOfdLy1byjBqvsygnQQHV0SRqQ5Gyx/Nu', 'active', 1, '2023-11-22 16:39:03', '2023-11-22 16:39:03');

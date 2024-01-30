c8198286ced04ffaff09922cf5f814a911f75820
vipinpandey3 <dev.virpandey@gmail.com>
2024-01-30 22:09:43 +0530

    #38 dashboard UI fixes

--- server/services/students.service.js
+++ server/services/students.service.js
@@ -6,29 +6,37 @@ const studentService = {
         try {
             console.log('Inside getStudentByIdFunction function');
             const studentId = req.params.id;
+            let studentDetails = [];
+            let object = {};
+            // if(student) {
+                
+                
             let student = await models.Student.findByPk(studentId, {
                 attributes: ['id', 'firstName', 'lastName']
             });
             student = JSON.parse(JSON.stringify(student));
-            let studentDetails = [];
-            let object = {}, studentAttendenceAll;
-            if(student) {
-                studentAttendenceAll = await models.StudentAttendence.findAll({where: {StudentId: studentId}})
-                object.fullName = student.firstName + " " + student.lastName;
-                object.id = student.id;
-                object.lastAttendenceDate = studentAttendenceAll.length > 0 ? studentAttendenceAll[studentAttendenceAll.length - 1].attendenceDate : "-";
-                object.lastInTime = studentAttendenceAll.length > 0 ? studentAttendenceAll[studentAttendenceAll.length - 1].inTime : "-";
-                studentDetails.push(object);
-                return {
-                    status: true,
-                    message: "Fetched student with last attendence",
-                    data: {
-                        studentDetails,
-                        attributes: attributes[23].columnsHeader
-                    }
+            console.log("student", student);
+            if (!student) {
+                throw new Error("Student with id: ", studentId, " is not not found");
+            }
+            let studentLastAttendence = await models.StudentAttendence.findOne({
+                where: {StudentId: studentId},
+                order: [['createdAt', 'DESC']],
+                limit: 1
+            })
+            studentLastAttendence = JSON.parse(JSON.stringify(studentLastAttendence))
+            object.fullName = student.firstName + " " + student.lastName;
+            object.StudentId = studentLastAttendence.StudentId;
+            object.lastAttendenceDate = studentLastAttendence && studentLastAttendence.attendenceDate ? studentLastAttendence.attendenceDate : "-" ;
+            object.lastInTime = studentLastAttendence && studentLastAttendence.inTime ? studentLastAttendence.inTime.inTime: "-";
+            studentDetails.push(object);
+            return {
+                status: true,
+                message: "Fetched student with last attendence",
+                data: {
+                    studentDetails,
+                    attributes: attributes[23].columnsHeader
                 }
-            } else {
-                throw new Error("No student found with Id: ", studentId)
             }
         } catch (error) {
             console.log("Error Inside the getStudentById", error);
